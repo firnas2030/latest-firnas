bc79d53e35bb9b1792d8612b464be61cfbb8b61c
firnas2030 <admin@firnas.aero>
2024-11-13 22:15:57 +0300

    Updattte InferenceForm.js

--- components/InferenceForm/InferenceForm.js
+++ components/InferenceForm/InferenceForm.js
@@ -276,7 +276,7 @@ function InferenceForm({
           }
           handleReverseGeocode(latitude, longitude);
           const response1 = await axios.post(
-            'https://firnas-backend-reryw95yx-firnas-projects-053b4e12.vercel.app/predict',
+            'https://18.232.138.189//predict',
             formData,
             {
               headers: {
